7eeebb06a0cf5ba5325d0925cc20ee05ff2ac5b0
Magnus <nharnharyhaw984@gmail.com>
2024-12-02 21:22:35 +0000

    added file path attribute to File model

--- models/file.py
+++ models/file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for file document
 """
-from mongoengine import StringField, DateTimeField, EnumField, ObjectIdField, Document
+from mongoengine import StringField, DateTimeField, EnumField, ObjectIdField, Document, FileField
 from util._enum import File_type
 from datetime import datetime
 from bson import ObjectId
@@ -12,7 +12,8 @@ class File(Document):
     """
     id = ObjectIdField(primary_key=True, default=ObjectId)
     name = StringField(required=True)
-    file_type = EnumField(File_type)
+    file_type = EnumField(File_type, required=True)
+    file_path = FileField(required=True)
     date_modified = DateTimeField(default=datetime.now)
 
     meta = {
