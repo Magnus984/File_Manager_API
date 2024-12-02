e6e0cfa47a1f15d112dea7e3fdfdbf1725645fd7
Magnus <nharnharyhaw984@gmail.com>
2024-12-02 21:21:30 +0000

    added new config settings to app

--- app.py
+++ app.py
@@ -4,8 +4,9 @@
 from fastapi import FastAPI
 from views.users import router as users_router
 from auth import router as auth_router
+from config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.PROJECT_TITLE)
 
 app.include_router(users_router)
 app.include_router(auth_router)
