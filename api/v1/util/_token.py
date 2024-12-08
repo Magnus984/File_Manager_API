f3104a298381c5035a7bf7398b30ebe50cc6c0e1
Magnus <nharnharyhaw984@gmail.com>
2024-12-08 17:38:45 +0000

    Mod: removed print debug statements

--- api/v1/util/_token.py
+++ api/v1/util/_token.py
@@ -38,13 +38,11 @@ def confirm_token(token: str, expected_type: str) -> str:
             )
         return email
     except jwt.ExpiredSignatureError:
-        print("token expired")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Token has expired"
         )
     except jwt.InvalidTokenError:
-        print("token invalid")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid token"
